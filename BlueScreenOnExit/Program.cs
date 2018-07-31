ae74358184e265795ac0fe077e49b71e200fb4fc
malcomvetter <malcomvetter@gmail.com>
2018-07-31 11:12:30 -0500

    init

---
+++ BlueScreenOnExit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!isAdmin())
+            {
+                Console.WriteLine("Go away user. Come back with your admin.");
+                Console.ReadLine();
+                return;
+            }
+            ProcessProtection.ProcessProtection.Protect();
+            Console.WriteLine("I'm a protected process, try to kill me.");
+            Console.ReadLine();
+            ProcessProtection.ProcessProtection.Unprotect();
+            Console.WriteLine("Whew! It's safe now.");
+        }
+
+        private static bool isAdmin()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent())).IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# BlueScreenOnExit
+
+Build in Visual Studio (.net 3.5 so it runs on Win 7+). Fire up as admin, attempt to kill the process, and it will BlueScreen the host.
+
+Not recommended for production systems. Use at your own risk.
